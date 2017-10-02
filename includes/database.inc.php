bf1d1b5d4ceed36cecddfafb564cf422fb83d603
Peter Myrvig <myrvig@gmail.com>
2017-10-02 22:17:32 +0200

    class DataBase

---
+++ includes/database.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+class DataBase {
+    //protected $hidden = [];
+    /**
+     * DB Table name
+     * $var string
+     */
+    private $tableName = "";
+    private $class = "";
+    /**
+     * Array of row names
+     * $var array
+     */
+    private $columns = [];
+    //private $noUse
+    
+    public function __construct() {
+        $this->class = get_class( $this);
+        $this->tableName = str
+    }
+    
+    
+    
+}
