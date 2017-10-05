564e7e195dfdbe3456052cf53fa4b39c71a79985
Peter Myrvig <myrvig@gmail.com>
2017-10-05 20:17:33 +0200

    new lines in city.php

--- Controllers/city.php
+++ Controllers/city.php
@@ -5,22 +5,22 @@
  */
 
 class City {
-    private $country;
+    private $countrycode;
     private $district;
     private $id;
     private $name;
     private $population;
 
-    public function __construct($cityCountry, $cityDistrict, $cityID, $cityName, $cityName, $cityPopulation) {
-        $this->country = $cityCountry;
+    public function __construct($cityCountryCode, $cityDistrict, $cityID, $cityName, $cityName, $cityPopulation) {
+        $this->countrycode = $cityCountryCode;
         $this->district = $cityDistrict;
         $this->id = $cityName;
         $this->name = $cityDistrict;
         $this->population = $cityPopulation;
     }
 
-    public function getCountry() {
-        return $this->country;
+    public function getCountryCode() {
+        return $this->countrycode;
     }
 
     public function getDistrict() {
@@ -39,14 +39,17 @@ class City {
         return $this->population;
     }
 
-    public static function setCreateCity($country, $district, $name, $population, $dbh) {
+    public static function setCreateCity($name, $countrycode, $district, $population, $dbh) {
         $CreateCity = array();
 
-        $sql = "INSERT INTO city (name, district, population)";
+        $sql = "INSERT INTO city (name, countrycode, district, population)";
         $sql .= "VALUES ($name, $district, $population)";
         try {
             $q = $dbh->prepare($sql);
-            $q->bindValue(':')
+            $q->bindValue(':name', $name);
+            $q->bindValue('district', $district);
+            $q->bindValue('population', $population);
+
         }
     }
 
