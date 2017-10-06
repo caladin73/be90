2400990638a3e5b0d0928126fea31f381813305c
Peter Myrvig <myrvig@gmail.com>
2017-10-06 11:06:51 +0200

    small changes in city.php

--- Controllers/city.php
+++ Controllers/city.php
@@ -5,42 +5,49 @@
  */
 
 class City {
+    private $city_id;
     private $city_name;
     private $city_countrycode;
     private $city_district ;
     private $city_population;
 
-    public function __construct($name, $countrycode, $district, $population) {
+    public function __construct($id, $name, $countrycode, $district, $population) {
+        $this->city_id;
         $this->city_name = $name;
         $this->city_countrycode= $countrycode;
         $this->city_district = $district;
         $this->city_population = $population;
     }
 
+    public function getCity_id() {
+        return $this->city_id;
+    }
+
     public function getName() {
         return $this->city_name;
     }
 
-    public function getCity_countrycode() {
+    public function getCity_Countrycode() {
         return $this->city_countrycode;
     }
 
-    public function getName() {
-        return $this->name;
+    public function getCity_District() {
+        return $this->city_district;
     }
 
-    public function getPopulation() {
-        return $this->population;
+    public function getCity_Population() {
+        return $this->city_population;
     }
 
     public static function setCreateCity($name, $countrycode, $district, $population, $dbh) {
         $CreateCity = array();
 
         $sql = "INSERT INTO city (name, countrycode, district, population)";
-        $sql .= "VALUES ($name, $district, $population)";
+        $sql .= "VALUES ($name, $countrycode, $district, $population)";
         try {
             $q = $dbh->prepare($sql);
             $q->bindValue(':name', $name);
+            $q->bindValue(':countrycode', countrycode);
             $q->bindValue('district', $district);
             $q->bindValue('population', $population);
 
